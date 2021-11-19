700ddf6fb54d4a9c4137169c34b89ab04cd9b1d3
bredlix <91148776+bredlix@users.noreply.github.com>
2021-11-19 19:32:43 +0200

    last api upd

--- hw_final/api_to_dwh_pipeline.py
+++ hw_final/api_to_dwh_pipeline.py
@@ -63,11 +63,37 @@ def dhop_oos_to_silver(**kwargs):
     logging.info(f"{df.count()} rows written")
     logging.info("Successfully loaded")
 
+
+def dhop_oos_to_dwh(**kwargs):
+
+    etl_date = kwargs.get('ds')
+
+    spark = SparkSession.builder \
+        .config('spark.driver.extraClassPath'
+                , '/home/user/shared_folder/postgresql-42.3.1.jar') \
+        .master('local')\
+        .appName('dshop_oos_to_dwh')\
+        .getOrCreate()
+
+    gp_con = BaseHook.get_connection('gp_dwh')
+    gp_url = f'jdbc:postgresql://{gp_con.host}:{gp_con.port}/postgres'
+    gp_creds = {'user': gp_con.login, 'password': gp_con.password}
+
+    df = spark.read.parquet(os.path.join('/', 'datalake', 'bronze', 'out_of_stock_api', f'{etl_date}'))
+
+    logging.info(f'Writing Out of stock product list for {etl_date} into DWH')
+
+    df.write.jdbc(gp_url, table='out_of_stock', properties=gp_creds)
+
+    logging.info(f"{df.count()} rows written")
+    logging.info("Successfully loaded")
+
+
 dag = DAG(
     dag_id='api_dwh_pipeline',
-    description='Export api dumps to HDFS',
+    description='API Processing Pipeline',
     schedule_interval='@daily',
-    start_date=datetime(2021, 11, 8, 12, 00)
+    start_date=datetime(2021, 11, 17, 12, 00)
 )
 
 start = DummyOperator(dag=dag, task_id='transfer_start')
@@ -86,4 +112,10 @@ api_data_to_silver = PythonOperator(
     python_callable=dhop_oos_to_silver,
     provide_context=True)
 
-start >> api_data_to_bronze >> end >> api_data_to_silver >> end2
+api_data_to_dwh = PythonOperator(
+    task_id='dhop_oos_to_dwh',
+    dag=dag,
+    python_callable=dhop_oos_to_dwh,
+    provide_context=True)
+
+start >> api_data_to_bronze >> end >> api_data_to_silver >> end2 >> api_data_to_dwh
